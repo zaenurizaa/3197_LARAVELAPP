5c09d1077883ed75d532206a708dfca3a3c0bb4f
zaenurizaa <zaenurizafaa@students.amikom.ac.id>
2026-07-26 23:55:40 +0700

    Make MySQL connection SSL optional for Clever Cloud integration

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => (extension_loaded('pdo_mysql') && env('DB_SSL', false)) ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA', '/etc/pki/tls/certs/ca-bundle.crt'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', false),
             ]) : [],
